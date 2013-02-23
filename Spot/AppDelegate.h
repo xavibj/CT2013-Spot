737e94068934d1379133ca849f1a4564592099d3
Walter Bogers <wbosoftware@gmail.com>
2013-02-23 22:43:12 +0100

    Initial Commit

---
+++ Spot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Spot
+//
+//  Created by W J Bogers on 23-02-13.
+//  Copyright (c) 2013 WboSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Spot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Spot
+//
+//  Created by W J Bogers on 23-02-13.
+//  Copyright (c) 2013 WboSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
